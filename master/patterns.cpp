4d885392447e617dcda3a04e04b1a128e33c6956
Damien <damien@kite>
2011-08-06 17:56:08 +0930

    Added a pattern that has fading moving vertically.  It reveals a bug with the first two columns!

--- master/patterns.cpp
+++ master/patterns.cpp
@@ -18,6 +18,15 @@ uint8_t fading_pattern(uint8_t c, uint8_t color, uint8_t col, uint8_t row) {
 		return 0;
 }
 
+uint8_t fading_vertical_pattern(uint8_t c, uint8_t color, uint8_t col, uint8_t row) {
+	static uint8_t lit = 0;
+
+	if (c == 0 && col == 0 && row == 0)
+		lit = (lit + 1) & 0x07;
+
+	return data[(row & 0x07) + lit];
+}
+
 uint8_t diagonal_pattern(uint8_t c, uint8_t color, uint8_t col, uint8_t row) {
 	return col - color == row ? 0xFF : 0;
 }
--- master/patterns.h
+++ master/patterns.h
@@ -6,11 +6,12 @@
 typedef uint8_t (*patternHandler)(uint8_t, uint8_t, uint8_t, uint8_t);
 
 uint8_t fading_pattern(uint8_t, uint8_t, uint8_t, uint8_t);
+uint8_t fading_vertical_pattern(uint8_t, uint8_t, uint8_t, uint8_t);
 uint8_t diagonal_pattern(uint8_t, uint8_t, uint8_t, uint8_t);
 
 const patternHandler pattern_handlers[] = {
 		fading_pattern,
-		diagonal_pattern,
+		fading_vertical_pattern,
 		diagonal_pattern,
 		diagonal_pattern,
 		diagonal_pattern,
