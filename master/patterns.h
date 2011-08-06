41711cee36713aa721032c75e52ca73bdf6147c9
Damien <damien@kite>
2011-08-06 18:01:40 +0930

    Rearranged the demo patterns, because some slots are unavailable on the Arduino

--- master/patterns.h
+++ master/patterns.h
@@ -10,11 +10,11 @@ uint8_t fading_vertical_pattern(uint8_t, uint8_t, uint8_t, uint8_t);
 uint8_t diagonal_pattern(uint8_t, uint8_t, uint8_t, uint8_t);
 
 const patternHandler pattern_handlers[] = {
+		diagonal_pattern, // Pattern shown when port B is disconnected
+		diagonal_pattern, // Unavailable on Arduinos
+		diagonal_pattern, // Unavailable on Arduinos
 		fading_pattern,
 		fading_vertical_pattern,
-		diagonal_pattern,
-		diagonal_pattern,
-		diagonal_pattern,
 		diagonal_pattern,
 		diagonal_pattern,
 		diagonal_pattern,
