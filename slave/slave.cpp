d5e759970b90cd58abd7f4e7f1ca39f9f43ce2d1
Damien <damien@kite>
2011-08-27 17:06:22 +0930

    Removed the row remappings, we'll do that before sending the data to the lantern
    This reverts commit d5c914b0ddbeb16bee82a77399567c0e72b699f6.

--- slave/slave.cpp
+++ slave/slave.cpp
@@ -42,18 +42,6 @@ uint16_t lookup[256];
 // mid-range values darker
 #define BRIGHTNESS 3
 
-// The mapping from physical rows to their memory locations
-uint8_t* const rows[] = {
-		tlc_data + 3 * NUM_TLCS * 24,
-		tlc_data + 2 * NUM_TLCS * 24,
-		tlc_data + 1 * NUM_TLCS * 24,
-		tlc_data + 0 * NUM_TLCS * 24,
-		tlc_data + 7 * NUM_TLCS * 24,
-		tlc_data + 6 * NUM_TLCS * 24,
-		tlc_data + 5 * NUM_TLCS * 24,
-		tlc_data + 4 * NUM_TLCS * 24,
-};
-
 // Events
 // Keep the events in a low register, for fast access
 #define events GPIOR0
@@ -68,7 +56,7 @@ void updateRow() {
 	++row;
 	if (row >= ROWS)
 		row = 0;
-	tlc_GSData = rows[row];
+	tlc_GSData = tlc_data + row * NUM_TLCS * 24;
 	Tlc.update();
 }
 
