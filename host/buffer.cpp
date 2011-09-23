33d/peel-lantern

commit e4909aa9e53657939b580a81b6a8a137d4985e88

Some hideous buffer initialization code

host/buffer.cpp
@@ -1,4 +1,6 @@
 #include "buffer.h"
+#include <unistd.h>
+#include <algorithm>
 
 //static int _skip_rows[] = { 4, 13, 22, 31 };
 //static int _skip_cols[] = { 8, 17, 28 };
@@ -9,6 +11,25 @@ Buffer::Buffer(int cols, int rows,
 		skip_rows(skip_rows),
 		skip_cols(skip_cols),
 		rows(rows), cols(cols),
-		// rows * 2 is because there's 2 TLCs per row
-		buf((cols - skip_cols.size()) * (rows - skip_rows.size()) + (rows * 2))
-{ }
+		tlc_start(1), tlc_end(13),
+		out_cols(cols - skip_cols.size()),
+		out_rows(rows - skip_rows.size()),
+		harvesters_per_row(2),
+		tlcs_per_harvester(4),
+		harvester_bytes(97),
+		buf((out_cols * out_rows) + (out_rows * harvesters_per_row)) {
+	// Initialize the headers
+	for (int i = 0; i < out_rows; i++) {
+		buf[(i * 2) * harvester_bytes] = (((i / 8) * 2) << 4) | ((i % 8) << 1) | 1;
+		buf[(i * 2 + 1) * harvester_bytes] = (((i / 8) * 2 + 1) << 4) | ((i % 8) << 1) | 1;
+	}
+}
+
+ssize_t BufferOutput::write(int fd) {
+	ssize_t count = pos - buffer.buf.size();
+	ssize_t written = ::write(fd, buffer.buf.data(), count);
+	pos += written;
+	if (pos >= buffer.buf.size())
+		pos = 0;
+	return written;
+}

host/Makefile
@@ -1,0 +1,12 @@
+OBJECTS      = buffer.o
+TEST_OBJECTS = buffertest.o test.o
+
+CPPFLAGS     = -std=c++0x
+
+test: $(OBJECTS) $(TEST_OBJECTS)
+	$(CC) $(LDFLAGS) $(LOADLIBES) $(LDLIBS) -lcppunit -o test \
+		$(OBJECTS) $(TEST_OBJECTS)
+	./test
+
+clean:
+	-rm *.o

host/buffer.h
@@ -13,25 +13,37 @@ private:
 	const std::set<int> skip_cols;
 	const int rows;
 	const int cols;
-	std::vector<uint8_t> const buf;
+	const int out_rows;
+	const int out_cols;
+	const int harvester_bytes;
+	const int tlc_start; // inclusive
+	const int tlc_end; // exclusive
+	const int harvesters_per_row;
+	const int tlcs_per_harvester;
+	/** The data buffer, as sent to the slaves*/
+	std::vector<uint8_t> buf;
 
 public:
 	Buffer(int cols, int rows,
 			std::initializer_list<int> skip_cols,
 			std::initializer_list<int> skip_rows);
 
-friend class BufferWriter;
+friend class BufferOutput;
+#if defined(CPPUNIT_ASSERT)
+friend class BufferTest;
+#endif
 };
 
-class BufferWriter {
+class BufferOutput {
 private:
-	Buffer const& buffer;
+	const Buffer & buffer;
+	int pos;
 public:
-	BufferWriter(Buffer& buffer) : buffer(buffer) {}
+	BufferOutput(const Buffer& buffer) : buffer(buffer), pos(0) {}
 	/**
-	 * Writes as much of the TLC buffer to some file descriptor as possible.
+	 * Writes as much of the TLC buffer as possible to some file descriptor.
 	 */
-	ssize_t write(int fd) const;
+	ssize_t write(int fd);
 };
 
 #endif /* BUFFER_H_ */

host/buffertest.cpp
@@ -1,0 +1,31 @@
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestSuite.h>
+#include <cstdio>
+#include "buffer.h"
+
+class BufferTest : public CppUnit::TestFixture  {
+private:
+
+public:
+	static CppUnit::Test *suite() {
+		CppUnit::TestSuite *suite = new CppUnit::TestSuite("BufferTest");
+		suite->addTest(new CppUnit::TestCaller<BufferTest>(
+				"testHeadersNoSkip",
+				&BufferTest::testHeadersNoSkip) );
+		return suite;
+	}
+
+	void testHeadersNoSkip() {
+		Buffer buf(32, 32, {}, {});
+		CPPUNIT_ASSERT_EQUAL(32, buf.out_rows);
+		CPPUNIT_ASSERT(buf.buf[      0] == 0x01);
+		CPPUNIT_ASSERT(buf.buf[     97] == 0x11);
+		CPPUNIT_ASSERT(buf.buf[ 2 * 97] == 0x03);
+		CPPUNIT_ASSERT(buf.buf[ 3 * 97] == 0x13);
+		CPPUNIT_ASSERT(buf.buf[14 * 97] == 0x0F);
+		CPPUNIT_ASSERT(buf.buf[15 * 97] == 0x1F);
+		CPPUNIT_ASSERT(buf.buf[16 * 97] == 0x21);
+	}
+};

host/test.cpp
@@ -1,0 +1,9 @@
+#include <cppunit/ui/text/TestRunner.h>
+#include "buffertest.cpp"
+
+int main(int argc, char **argv) {
+	CppUnit::TextUi::TestRunner runner;
+	runner.addTest(BufferTest::suite());
+	runner.run();
+	return 0;
+}

commit c6c50cd59967a35aa556d39cfc31663d8a785b34

The wrong part of the output buffer was probably being written to stdout, introduced in e4909aa

host/buffer.cpp
@@ -27,7 +27,7 @@ Buffer::Buffer(int cols, int rows,
 
 ssize_t BufferOutput::write(int fd) {
 	ssize_t count = pos - buffer.buf.size();
-	ssize_t written = ::write(fd, buffer.buf.data(), count);
+	ssize_t written = ::write(fd, buffer.buf.data() + count, count);
 	pos += written;
 	if (pos >= buffer.buf.size())
 		pos = 0;